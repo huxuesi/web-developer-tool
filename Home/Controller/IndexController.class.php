d0dc448a456a8a03cc83e5cec3e1c1cc1c96e5c5
eternity <eternity@eternity>
2017-01-07 12:02:53 +0800

    huxuesi

--- Home/Controller/IndexController.class.php
+++ Home/Controller/IndexController.class.php
@@ -2,11 +2,40 @@
 namespace Home\Controller;
 use Think\Controller;
 class IndexController extends PublicController {
-  public function index(){
-  	$ipaddress = M("ipaddress");
-		$res = $ipaddress->select();
-		dump($res);
+	public function index() {
+		$ipaddress = M("ipaddress");
+
+		if (isset($_SERVER)) {
+			if (isset($_SERVER["HTTP_X_FORWARDED_FOR"])) {
+				$IPaddress = $_SERVER["HTTP_X_FORWARDED_FOR"];
+			} else if (isset($_SERVER["HTTP_CLIENT_IP"])) {
+				$IPaddress = $_SERVER["HTTP_CLIENT_IP"];
+			} else {
+				$IPaddress = $_SERVER["REMOTE_ADDR"];
+			}
+		} else {
+			if (getenv("HTTP_X_FORWARDED_FOR")) {
+				$IPaddress = getenv("HTTP_X_FORWARDED_FOR");
+			} else if (getenv("HTTP_CLIENT_IP")) {
+				$IPaddress = getenv("HTTP_CLIENT_IP");
+			} else {
+				$IPaddress = getenv("REMOTE_ADDR");
+			}
+		}
+
+		$taobaoIP = 'http://ip.taobao.com/service/getIpInfo.php?ip='.$IPaddress;
+    $IPinfo = json_decode(file_get_contents($taobaoIP));
+    $province = $IPinfo->data->region;
+    $city = $IPinfo->data->city;
+    $address = $province.$city;
 		
-  	echo "这是首页！";
+		$data['ip'] = ip2long($IPaddress);
+		$data['address'] = $address;
+		$data['time'] = time();
+		$ipaddress -> add($data);
+		
+		echo "本站提供各种工具！";
 	}
+
+
 }
