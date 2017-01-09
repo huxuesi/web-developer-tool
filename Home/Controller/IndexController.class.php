63f926b3ba98d68b87e098943b5ea7fdc516ecb2
eternity <eternity@eternity>
2017-01-09 14:11:56 +0800

    huxuesi

--- Home/Controller/IndexController.class.php
+++ Home/Controller/IndexController.class.php
@@ -3,48 +3,8 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends PublicController {
 	public function index() {
-		$ipaddress = M("ipaddress");
-
-		if (isset($_SERVER)) {
-			if (isset($_SERVER["HTTP_X_FORWARDED_FOR"])) {
-				$IPaddress = $_SERVER["HTTP_X_FORWARDED_FOR"];
-			} else if (isset($_SERVER["HTTP_CLIENT_IP"])) {
-				$IPaddress = $_SERVER["HTTP_CLIENT_IP"];
-			} else {
-				$IPaddress = $_SERVER["REMOTE_ADDR"];
-			}
-		} else {
-			if (getenv("HTTP_X_FORWARDED_FOR")) {
-				$IPaddress = getenv("HTTP_X_FORWARDED_FOR");
-			} else if (getenv("HTTP_CLIENT_IP")) {
-				$IPaddress = getenv("HTTP_CLIENT_IP");
-			} else {
-				$IPaddress = getenv("REMOTE_ADDR");
-			}
-		}
-
-		$taobaoIP = 'http://ip.taobao.com/service/getIpInfo.php?ip='.$IPaddress;
-		
-		$curl = curl_init();
-	  curl_setopt($curl, CURLOPT_URL, $taobaoIP);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-	  $output = curl_exec($curl);
-	  curl_close($curl);
-		
-    $IPinfo = json_decode($output);
-		if( !empty($IPinfo) ){
-			$province = $IPinfo->data->region;
-	    $city = $IPinfo->data->city;
-	    $address = $province.$city;
-		}else{
-			$address = "获取地址失败";
-		}
-
-		$data['ip'] = ip2long($IPaddress);
-		$data['address'] = $address;
-		$data['time'] = time();
-		$ipaddress -> add($data);
-
+		$ipaddress = new \Model\ipaddressModel();
+		$ipaddress->index();
 		$this->display();
 	}
 
---
+++ Model/ipaddressModel.class.php
@@ -0,0 +1,51 @@
+<?php
+namespace Model;
+use Think\Model;
+class ipaddressModel extends Model {
+	public function index() {
+		if( empty(session('isfirst')) ){
+			$ipaddress = M("ipaddress");
+			if (isset($_SERVER)) {
+				if (isset($_SERVER["HTTP_X_FORWARDED_FOR"])) {
+					$IPaddress = $_SERVER["HTTP_X_FORWARDED_FOR"];
+				} else if (isset($_SERVER["HTTP_CLIENT_IP"])) {
+					$IPaddress = $_SERVER["HTTP_CLIENT_IP"];
+				} else {
+					$IPaddress = $_SERVER["REMOTE_ADDR"];
+				}
+			} else {
+				if (getenv("HTTP_X_FORWARDED_FOR")) {
+					$IPaddress = getenv("HTTP_X_FORWARDED_FOR");
+				} else if (getenv("HTTP_CLIENT_IP")) {
+					$IPaddress = getenv("HTTP_CLIENT_IP");
+				} else {
+					$IPaddress = getenv("REMOTE_ADDR");
+				}
+			}
+	
+			$taobaoIP = 'http://ip.taobao.com/service/getIpInfo.php?ip='.$IPaddress;
+			
+			$curl = curl_init();
+		  curl_setopt($curl, CURLOPT_URL, $taobaoIP);
+	    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+		  $output = curl_exec($curl);
+		  curl_close($curl);
+			
+	    $IPinfo = json_decode($output);
+			if( !empty($IPinfo) ){
+				$province = $IPinfo->data->region;
+		    $city = $IPinfo->data->city;
+		    $address = $province.$city;
+			}else{
+				$address = "获取地址失败";
+			}
+			
+			$data['ip'] = ip2long($IPaddress);
+			$data['address'] = $address;
+			$data['time'] = time();
+			$ipaddress -> add($data);
+		}
+
+		session('isfirst','false');
+	}
+}
