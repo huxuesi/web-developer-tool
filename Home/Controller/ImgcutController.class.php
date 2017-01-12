f4f25e1c2b22a68041fdf838613013c3d72a45ed
eternity <eternity@eternity>
2017-01-12 20:18:05 +0800

    1.0

--- Home/Controller/ImgcutController.class.php
+++ Home/Controller/ImgcutController.class.php
@@ -22,7 +22,7 @@ class ImgcutController extends PublicController {
 	}
 	
 	public function imgcut(){
-		/*if( is_numeric( I('post.width') ) && is_numeric( I('post.height') ) && is_numeric( I('post.x1') ) && is_numeric( I('post.y1') ) && I('post.imgurl')!='' ){
+		if( is_numeric( I('post.width') ) && is_numeric( I('post.height') ) && is_numeric( I('post.x1') ) && is_numeric( I('post.y1') ) && I('post.imgurl')!='' ){
 			$imgurl = I('post.imgurl');
 			$image = new \Think\Image(\Think\Image::IMAGE_IMAGICK);
 			$image->open( './'.$imgurl );
@@ -35,8 +35,8 @@ class ImgcutController extends PublicController {
 			
 			$image->crop(I('post.width'), I('post.height'), I('post.x1'), I('post.y1'))->save('./'.$newurl);
 			$this->ajaxReturn( $newurl );
-		}else{*/
+		}else{
 			$this->ajaxReturn( 404 );
-		//}
+		}
 	}
 }
