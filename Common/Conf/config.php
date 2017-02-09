c8a2acb5e75c88a996dca1f4b53ec42def508dab
eternity <eternity@eternity>
2017-02-09 17:09:01 +0800

    1.0

--- Common/Conf/config.php
+++ Common/Conf/config.php
@@ -40,11 +40,11 @@ return array(
 	// 数据库设置
 	'DB_TYPE'               =>  'mysql',     	// 数据库类型
 	'DB_HOST'               =>  'localhost', 	// 服务器地址
-	'DB_NAME'               =>  'tool',   			// 数据库名
-	'DB_USER'               =>  'tool',      	// 用户名
-	'DB_PWD'                =>  '1R7c&etxKTafkc9r-tool',      	// 密码
+	'DB_NAME'               =>  '1',   			// 数据库名
+	'DB_USER'               =>  '1',      	// 用户名
+	'DB_PWD'                =>  '1',      	// 密码
 	'DB_PORT'               =>  '3306',      	// 端口
-	'DB_PREFIX'             =>  'hxs_',     		// 数据库表前缀
+	'DB_PREFIX'             =>  '1',     		// 数据库表前缀
 	'DB_CHARSET'			  		=>  'utf8',	 		 	//数据库编码
 
 	//开启Smarty模板引擎
