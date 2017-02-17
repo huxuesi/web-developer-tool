6f8437795b3f0ca2d4e66d3d6ae4a0e979fb266d
eternity <eternity@eternity>
2017-02-17 17:19:27 +0800

    1.0

--- Public/js/style.js
+++ Public/js/style.js
@@ -2,6 +2,6 @@ $(function(){
 	$(".xs-nav-top").autoHidingNavbar({animationDuration:500})
 	$('.imgcutyes').on('click', '#download', function(){
 		var url = $(this).attr('imgurl')
-		window.location.href = "http://tool.akphp.com/index.php/Imgcut/imgdownload.html/remoteurl/"+url
+		window.location.href = "http://tool.akphp.com/index.php/Imgcut/imgdownload.html?remoteurl="+url
 	})
 })
