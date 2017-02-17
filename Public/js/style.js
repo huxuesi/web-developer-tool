28919e9adced6bb321796a7262d2181a871549a5
eternity <eternity@eternity>
2017-02-17 16:59:14 +0800

    1.0

--- Public/js/style.js
+++ Public/js/style.js
@@ -2,6 +2,6 @@ $(function(){
 	$(".xs-nav-top").autoHidingNavbar({animationDuration:500})
 	$('.imgcutyes').on('click', '#download', function(){
 		var url = $(this).attr('imgurl')
-		alert(url)
+		window.open(url)
 	})
 })
