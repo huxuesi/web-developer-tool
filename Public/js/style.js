91c7a0ad5885deaad42fea4cc91901d22637d876
eternity <eternity@eternity>
2017-02-17 17:09:44 +0800

    1.0

--- Public/js/style.js
+++ Public/js/style.js
@@ -2,6 +2,10 @@ $(function(){
 	$(".xs-nav-top").autoHidingNavbar({animationDuration:500})
 	$('.imgcutyes').on('click', '#download', function(){
 		var url = $(this).attr('imgurl')
-		window.open(url)
+		
+				objIframe = document.createElement("IFRAME"); 
+				document.body.insertBefore(objIframe); 
+				objIframe.outerHTML = "<iframe name=a1 style='width:400px;hieght:300px' src=" + url + "></iframe>"; 
+
 	})
 })
