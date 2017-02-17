58b035d4b7610b0c585ef8892e1c3f84ef6f4801
eternity <eternity@eternity>
2017-02-17 15:30:31 +0800

    1.0

--- Home/Controller/ImgcutController.class.php
+++ Home/Controller/ImgcutController.class.php
@@ -27,7 +27,7 @@ class ImgcutController extends PublicController {
 			$image->open( './'.$imgurl );
 			$w = $image->width();
 			$h = $image->height();
-			$image->thumb($w, $h, \Think\Image::IMAGE_THUMB_FIXED)->save('./'.$newurl, null, $num);
+			$image->thumb(100, 100, \Think\Image::IMAGE_THUMB_FIXED)->save('./'.$newurl, null, $num);
 			$this->ajaxReturn( ltrim($newurl, './') );
 		}else{
 			$this->ajaxReturn( 404 );
