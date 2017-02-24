3406913bcb5ef51b642037a7318f23636fabcc4d
eternity <eternity@eternity>
2017-02-24 17:01:05 +0800

    1.0

--- Public/js/style.js
+++ Public/js/style.js
@@ -1,3 +1,6 @@
 $(function(){
+	document.querySelector(".list-group-item[data-hover='"+currnav+"']").className += " active"
+	$(".list-group-item[data-hover='"+currnav+"']").parent('.collapse').collapse('show')
+
 	$(".xs-nav-top").autoHidingNavbar({animationDuration:500})
 })
