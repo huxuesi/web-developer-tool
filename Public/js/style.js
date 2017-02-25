52635f306eeefaf301c1a4892f660f870e5a6350
eternity <eternity@eternity>
2017-02-25 21:18:25 +0800

    1.0

--- Public/js/style.js
+++ Public/js/style.js
@@ -3,7 +3,7 @@ $(function(){
 		$(".list-group-item[data-hover='"+currnav+"']").addClass('active')
 		$(".list-group-item[data-hover='"+currnav+"']").parent('.collapse').collapse('show')
 	}else{
-		$('#collapse1').collapse('show')
+		$('.xs-collapse-a').collapse('show')
 	}
 
 	$(".xs-nav-top").autoHidingNavbar({animationDuration:500})
