abaa05cca6e16853d267a81d4fa43c946780a7eb
eternity <eternity@eternity>
2017-02-25 21:15:11 +0800

    1.0

--- Public/js/style.js
+++ Public/js/style.js
@@ -1,6 +1,10 @@
 $(function(){
-	$(".list-group-item[data-hover='"+currnav+"']").addClass('active')
-	$(".list-group-item[data-hover='"+currnav+"']").parent('.collapse').collapse('show')
+	if( currnav != '' ){
+		$(".list-group-item[data-hover='"+currnav+"']").addClass('active')
+		$(".list-group-item[data-hover='"+currnav+"']").parent('.collapse').collapse('show')
+	}else{
+		$('.collapse').eq(0).collapse('show')
+	}
 
 	$(".xs-nav-top").autoHidingNavbar({animationDuration:500})
 })
